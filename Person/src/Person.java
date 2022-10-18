3b6e85cc492a2d5a284bdeffb305000184894cd6
Lorenz Hankel <lorenz.hankel@yahoo.de>
2022-10-18 12:42:03 +0200

    Update Person.java
    Methoden für Validierung implementiert

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -1,6 +1,12 @@
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Person {
+
+	public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
+			Pattern.CASE_INSENSITIVE);
+	public static final Pattern VALID_PASSWORD_REGEX = Pattern.compile("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+			Pattern.CASE_INSENSITIVE);
 	private String name;
 	/**
 	 * Vor @ mindestens 1 Wort Nach @ mindestens 1 Wort Nach letzen Wort ein Punkt
@@ -17,8 +23,14 @@ public class Person {
 		this.email = email;
 		this.password = password;
 	}
-	
+
 	public boolean checkEmail(String email) {
-		return true;
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+		return matcher.find();
+	}
+
+	public boolean checkPassword(String password) {
+		Matcher matcher = VALID_PASSWORD_REGEX.matcher(password);
+		return matcher.find();
 	}
 }
