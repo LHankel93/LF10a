b7800500b9c1d1894610a98ee8f2691737ff1efd
Lorenz Hankel <lorenz.hankel@yahoo.de>
2022-12-12 23:34:41 +0100

    Grundfunktion A3_2 implementiert

--- A3_2/src/Frame.java
+++ A3_2/src/Frame.java
@@ -13,12 +13,13 @@ public class Frame implements ActionListener {
 
 	JFrame frame;
 	private JTextField txtLogin;
-	private JButton BtnClean;
+	private JButton btnLogin;
 	private JTextField txtPassword;
 	private JLabel lblLogin;
 	private JLabel lblPassword;
 	private JPanel panelBox1;
 	private JPanel panelBox2;
+	private JPanel panelBox3;
 
 	public Frame() {
 		initialize();
@@ -30,44 +31,55 @@ public class Frame implements ActionListener {
 		frame.setBounds(100, 100, 450, 183);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
+		// Panel deklarieren
 		panelBox1 = new JPanel();
-		frame.getContentPane().add(panelBox1);
-		
+		frame.getContentPane().add(panelBox1, BorderLayout.NORTH);
+
 		panelBox2 = new JPanel();
-		frame.getContentPane().add(panelBox2);
-		
+		frame.getContentPane().add(panelBox2, BorderLayout.CENTER);
+
+		panelBox3 = new JPanel();
+		frame.getContentPane().add(panelBox3, BorderLayout.SOUTH);
+
 		lblLogin = new JLabel("Benutzername");
-		frame.getContentPane().add(lblLogin);
+		panelBox1.add(lblLogin);
+		// frame.getContentPane().add(lblLogin);
 
 		txtLogin = new JTextField();
 		txtLogin.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.getContentPane().add(txtLogin);
+		// frame.getContentPane().add(txtLogin);
 		txtLogin.setColumns(10);
-		
+		panelBox1.add(txtLogin);
+
 		lblPassword = new JLabel("Passwort");
-		frame.getContentPane().add(lblPassword);
-		
+		// frame.getContentPane().add(lblPassword);
+		panelBox2.add(lblPassword);
+
 		txtPassword = new JTextField();
 		txtPassword.setHorizontalAlignment(SwingConstants.CENTER);
 		txtPassword.setColumns(10);
-		frame.getContentPane().add(txtPassword);
-		
-				BtnClean = new JButton("Login");
-				BtnClean.addActionListener(this);
-				frame.getContentPane().add(BtnClean);
+		// frame.getContentPane().add(txtPassword);
+		panelBox2.add(txtPassword);
+
+		btnLogin = new JButton("Login");
+		btnLogin.addActionListener(this);
+		// frame.getContentPane().add(btnLogin);
+		panelBox3.add(btnLogin);
 	}
+
+	/*
+	 * Wenn der Login-Button gedrückt wird, soll auf die Konsule die folgende Text
+	 * ausgegeben werden:
+	 * 
+	 * Benutzername: <Inhalt des Textfeldes> Password: <Inhalt des Textfeldes>
+	 */
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == this.BtnClean) {
-			this.txtLogin.setText("0");
-		} else if (e.getSource() == this.BtnIncrement) {
-			Integer nums = Integer.parseInt(this.txtLogin.getText()) + 1;
-			this.txtLogin.setText(nums.toString());
-		} else if (e.getSource() == this.BtnDecrement) {
-			Integer nums = Integer.parseInt(this.txtLogin.getText()) - 1;
-			this.txtLogin.setText(nums.toString());
+		if (e.getSource() == this.btnLogin) {
+			System.out.println(
+					"Benutzername:\t\t" + this.txtLogin.getText() + "\nPassword:\t\t" + this.txtPassword.getText());
 		}
 	}
 
